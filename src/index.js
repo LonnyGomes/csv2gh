e4100807a63601dde4c23c5f1a1b641ffe91af6c
Lonny Gomes <code@lonnygomes.com>
2020-10-12 16:20:22 -0400

    feat: generate title, body, and labels content from data

--- src/index.js
+++ src/index.js
@@ -6,13 +6,54 @@ const octokit = new Octokit();
 
 const parser = new Parser('MOCK_DATA.csv');
 
+const genTitle = (data) => {
+    const { req_id, title } = data;
+    return `CTLR-${req_id} - ${title}`;
+};
+
+const genBody = (data) => {
+    const { title, public_impl, notes, priority } = data;
+
+    const msg = `## ${title}
+
+**Priority:** ${priority}
+
+### Public Implementation
+
+${public_impl}
+
+### Notes
+
+<details>
+  <summary>Additional Notes</summary>
+  <p>${notes}</p>
+</details>
+
+`;
+
+    return msg;
+};
+
+const genLabels = (data) => {
+    return data.type;
+};
+
 const init = async () => {
+    let results = null;
+
     try {
-        const results = await parser.parse();
-        console.log('results', results);
+        results = await parser.parse();
     } catch (error) {
         console.error(`Error encountered while parsing: ${error.message}`);
     }
+
+    results.forEach((item) => {
+        const title = genTitle(item);
+        const body = genBody(item);
+        const labels = genLabels(item);
+
+        console.log(title, body, labels);
+    });
 };
 
 init();
